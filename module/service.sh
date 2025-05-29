fe44c783d21541dce601f8b85333ba75c273d136
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-05-29 23:02:53 +0800

    Update service.sh

--- module/service.sh
+++ module/service.sh
@@ -4,12 +4,12 @@ MODDIR=${0%/*}
 MODULE_PROP="$MODDIR/module.prop"
 MOD_INTRO="A Magisk module to disguise the status of encryption as encrypted."
 
-. "$MODDIR/aautilities.sh"
+. "$MODDIR/aa-util.sh"
 
 while [ "$(getprop sys.boot_completed)" != "1" ]; do
     sleep 1
 done
 
-resetprop -n "ro.crypto.state" "encrypted"
+check_before_resetprop "ro.crypto.state" "encrypted"
 DESCRIPTION="[⚙️Data Partition state: $(getprop ro.crypto.state)] $MOD_INTRO"
-update_config_value "description" "$DESCRIPTION" "$MODULE_PROP"
+update_config_var "description" "$DESCRIPTION" "$MODULE_PROP"
