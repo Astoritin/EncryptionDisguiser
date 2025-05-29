550344c2fc48b03be623c827636c558a14dc12ef
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-05-29 23:02:20 +0800

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -9,14 +9,14 @@ MOD_INTRO="A Magisk module to disguise the status of encryption as encrypted."
 
 [ ! -d "$VERIFY_DIR" ] && mkdir -p "$VERIFY_DIR"
 
-echo "- Extract aautilities.sh"
-unzip -o "$ZIPFILE" 'aautilities.sh' -d "$TMPDIR" >&2
-if [ ! -f "$TMPDIR/aautilities.sh" ]; then
-  echo "! Failed to extract aautilities.sh!"
+echo "- Extract aa-util.sh"
+unzip -o "$ZIPFILE" 'aa-util.sh' -d "$TMPDIR" >&2
+if [ ! -f "$TMPDIR/aa-util.sh" ]; then
+  echo "! Failed to extract aa-util.sh!"
   abort "! This zip may be corrupted!"
 fi
 
-. "$TMPDIR/aautilities.sh"
+. "$TMPDIR/aa-util.sh"
 
 logowl "Setting up $MOD_NAME"
 logowl "Version: $MOD_VER"
@@ -24,14 +24,14 @@ install_env_check
 show_system_info
 logowl "Install from $ROOT_SOL app"
 logowl "Essential checks"
-extract "$ZIPFILE" 'aautilities.sh' "$VERIFY_DIR"
+extract "$ZIPFILE" 'aa-util.sh' "$VERIFY_DIR"
 extract "$ZIPFILE" 'customize.sh' "$VERIFY_DIR"
 logowl "Extract module files"
-extract "$ZIPFILE" 'aautilities.sh' "$MODPATH"
+extract "$ZIPFILE" 'aa-util.sh' "$MODPATH"
 extract "$ZIPFILE" 'module.prop' "$MODPATH"
 extract "$ZIPFILE" 'service.sh' "$MODPATH"
 logowl "Set permission"
 set_permission_recursive "$MODPATH" 0 0 0755 0644
 logowl "Welcome to use ${MOD_NAME}!"
-DESCRIPTION="[⏳Reboot to take effect.] $MOD_INTRO"
-update_config_value "description" "$DESCRIPTION" "$MODPATH/module.prop"
+DESCRIPTION="[✨Reboot to take effect.] $MOD_INTRO"
+update_config_var "description" "$DESCRIPTION" "$MODPATH/module.prop"
